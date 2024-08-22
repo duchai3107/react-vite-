c4a118af26b65d2d8d1efa3ed1e42b5f317fa347
Nguyễn Đức Hải <148833400+duchai3107@users.noreply.github.com>
2024-08-22 14:42:33 +0700

    video 31

--- src/components/learn/SecondComponent.jsx
+++ src/components/learn/SecondComponent.jsx
@@ -1,8 +1,14 @@
 import './style.css';
 const SecondComponent = () => {
+    const test = "hai oi hoc dy";
+    const teest1 = {
+        name: 'hai',
+        age: 34
+    }
     return (
         <>
-            <div>Anh Hai Dep Trai Qua</div>
+            <div>{JSON.stringify(teest1)}</div>
+            <div>{test} Anh Hai Dep Trai Qua</div>
             <div className="NewStyle" style={{ borderRadius: "20px" }}>HAi promax-mimimi</div>
         </>
     );
