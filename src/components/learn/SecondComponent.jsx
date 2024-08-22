aa819038c7b3efd2eb32235706ea47c725e02f47
Nguyễn Đức Hải <148833400+duchai3107@users.noreply.github.com>
2024-08-22 14:23:08 +0700

    video 30

--- src/components/learn/SecondComponent.jsx
+++ src/components/learn/SecondComponent.jsx
@@ -1,6 +1,10 @@
+import './style.css';
 const SecondComponent = () => {
     return (
-        <div>Anh Hai Dep Trai Qua</div>
+        <>
+            <div>Anh Hai Dep Trai Qua</div>
+            <div className="NewStyle" style={{ borderRadius: "20px" }}>HAi promax-mimimi</div>
+        </>
     );
 }
 export { SecondComponent };
